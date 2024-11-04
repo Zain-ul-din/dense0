17452840f75589d5bdf5af5be318a21e6d9e4cd5
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-04 09:00:22 +0500

    add novel

--- src/app/new-post/page.tsx
+++ src/app/new-post/page.tsx
@@ -1,3 +1,19 @@
-export default function NewPostPage() {
-  return <>Create new post</>;
-}
+"use client";
+
+import { defaultExtensions } from "@/components/editor/extensions";
+import { EditorContent, EditorRoot } from "novel";
+
+const Page = () => (
+  <>
+    <h1>Hello World</h1>
+    <EditorRoot>
+      <EditorContent
+        className="bg-white prose"
+        immediatelyRender={false}
+        extensions={defaultExtensions}
+      ></EditorContent>
+    </EditorRoot>
+  </>
+);
+
+export default Page;
