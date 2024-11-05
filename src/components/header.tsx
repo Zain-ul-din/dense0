4033d8c09a02e2d7f2c9651c31164b08156337b2
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-05 06:39:40 +0500

    add routing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
 import { ThemeToggle } from "./misc/theme-toggle";
+import { Button } from "./ui/button";
+import { Pencil } from "lucide-react";
+import { ROUTES } from "@/lib/constants";
 
 export default function Header() {
   return (
     <header className="px-4 py-6 border-b">
       <div className="max-w-screen-xl flex items-center mx-auto">
-        <h2 className="font-mono text-2xl">DENSE0</h2>
-        <nav className="flex-1 flex justify-end">
+        <Link href={ROUTES.home}>
+          <h2 className="font-mono text-2xl">DENSE0</h2>
+        </Link>
+        <nav className="flex-1 flex justify-end gap-2">
+          <Link href={ROUTES.new_post}>
+            <Button variant={"outline"}>
+              <Pencil className="icon" />
+              Write
+            </Button>
+          </Link>
           <ThemeToggle />
         </nav>
       </div>
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  home: "/",
+  new_post: "/new-post"
+};
