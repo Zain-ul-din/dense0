f1c697e8294502d93606be2bb6d8acd4a90b046d
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-11-06 20:00:54 +0500

    slug

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,5 +1,16 @@
 import { getPostById } from "@/lib/dal/post";
 import { notFound } from "next/navigation";
+import { generateHTML } from "@tiptap/html";
+
+import Bold from "@tiptap/extension-bold";
+import BulletList from "@tiptap/extension-bullet-list";
+import Document from "@tiptap/extension-document";
+import Heading from "@tiptap/extension-heading";
+import Link from "@tiptap/extension-link";
+import ListItem from "@tiptap/extension-list-item";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import { Container, Section } from "@/components/craft";
 
 export const dynamicParams = true;
 
@@ -16,8 +27,24 @@ export default async function Page({
   }
 
   return (
-    <>
-      <h1 className="text-7xl">{post._id}</h1>
-    </>
+    <Section>
+      <Container>
+        <div
+          className="prose dark:prose-invert max-w-screen-lg mx-auto"
+          dangerouslySetInnerHTML={{
+            __html: generateHTML(JSON.parse(post.json as any as string), [
+              Bold,
+              BulletList,
+              Document,
+              Heading,
+              Link,
+              Paragraph,
+              ListItem,
+              Text
+            ])
+          }}
+        />
+      </Container>
+    </Section>
   );
 }
